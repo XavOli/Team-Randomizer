f85e77116168b7e3c090a542b915de1fb088b8ed
Xavier O. <xavoli2099@gmail.com>
2024-10-30 10:02:47 -0400

    Initial commit from starter

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a tailwindcss + webpack starter.
+
+Build distribution files : npm run build
+Start development server on port 3000 : npm run dev
+
+Dev server compiles (inlcuding tailwind) on save, needs manual page reload in browser to see changes.
+
+Resources:
+https://tailwindcss.com/docs/installation
+https://gist.github.com/bradtraversy/1c93938c1fe4f10d1e5b0532ae22e16a
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack + Tailwind App',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
