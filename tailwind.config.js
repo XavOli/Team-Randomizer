08d24702d3da662fb619f8e1ded5013117f476cd
Xavier O. <xavoli2099@gmail.com>
2024-11-10 11:18:45 -0500

    Mostly color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./src/**/*.html', './src/**/*.js'],
   theme: {
     extend: {
+      colors: {
+        primary: '#1B1A55',
+        secondary: '#070F2B',
+        tertiaryDark: '#535C91',
+        tertiaryLight: '#9290C3',
+      },
       backgroundImage: {
         volley: "url('../img/volleyball.jpg')",
       },
